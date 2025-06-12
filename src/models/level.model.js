87572db39807b52a65d124cb1c428ddd4d0f8852
SUJAl <sva7422@gmail.com>
2025-06-12 13:21:37 +0530

    add user and level model

---
+++ src/models/level.model.js
@@ -0,0 +1,44 @@
+import mongoose,{ Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const levelSchema = new Schema(
+    {
+        Beginner: { type: Number, default: 0 },
+        Easy: { type: Number, default: 0 },
+        Trivial: { type: Number, default: 0 },
+        Introductory: { type: Number, default: 0 },
+        VeryEasy: { type: Number, default: 0 },
+        Light: { type: Number, default: 0 },
+        Basic: { type: Number, default: 0 },
+        Simple: { type: Number, default: 0 },
+        Casual: { type: Number, default: 0 },
+        Amateur: { type: Number, default: 0 },
+        Intermediate: { type: Number, default: 0 },
+        Regular: { type: Number, default: 0 },
+        Standard: { type: Number, default: 0 },
+        Moderate: { type: Number, default: 0 },
+        Tricky: { type: Number, default: 0 },
+        Challenging: { type: Number, default: 0 },
+        Skilled: { type: Number, default: 0 },
+        Hard: { type: Number, default: 0 },
+        Tough: { type: Number, default: 0 },
+        Advanced: { type: Number, default: 0 },
+        Difficult: { type: Number, default: 0 },
+        Expert: { type: Number, default: 0 },
+        Pro: { type: Number, default: 0 },
+        Intense: { type: Number, default: 0 },
+        Extreme: { type: Number, default: 0 },
+        Brutal: { type: Number, default: 0 },
+        Insane: { type: Number, default: 0 },
+        Nightmare: { type: Number, default: 0 },
+        Impossible: { type: Number, default: 0 },
+        Mythic: { type: Number, default: 0 },
+        Legendary: { type: Number, default: 0 },
+        Super_level0: { type: Number, default: 0 },
+        Super_level1: { type: Number, default: 0 }
+    }
+)
+
+levelSchema.plugin(mongooseAggregatePaginate)
+
+export const Level = mongoose.model("Level", levelSchema);
---
+++ src/models/user.model.js
@@ -0,0 +1,79 @@
+import mongoose,{ Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        Username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase:true,
+            trim: true,
+            index: true
+        },
+
+        Email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase:true,
+            trim: true,
+        },
+
+        levelhistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref:"Level"
+            }
+        ],
+
+        Password: {
+            type: String,
+            required: [true, 'passwprd is required']
+        },
+
+        Refreshtoken: {
+            type: string
+        }
+        
+    }
+)
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("Password")) return next();
+    this.Password =bcrypt.hash(this.Password, 4)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (Password) {
+   return await bcrypt.compare(Password, this.Password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            Email: thia.Email,
+            Username: this.Username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+            {
+                _id: this._id
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
